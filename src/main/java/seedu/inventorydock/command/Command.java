acada3ebe5a16b41d3b578e0de913d86ff3b6cf2
Yeo Si Zhao <sizhaoyeo1@gmail.com>
2026-04-05 23:32:36 +0800

    Update commands

--- src/main/java/seedu/inventorydock/command/Command.java
+++ src/main/java/seedu/inventorydock/command/Command.java
@@ -1,12 +1,12 @@
 package seedu.inventorydock.command;
 
-import seedu.inventorydock.exception.DukeException;
+import seedu.inventorydock.exception.InventoryDockException;
 import seedu.inventorydock.model.Inventory;
 import seedu.inventorydock.ui.UI;
 
 public abstract class Command {
 
-    public abstract void execute(Inventory inventory, UI ui) throws DukeException;
+    public abstract void execute(Inventory inventory, UI ui) throws InventoryDockException;
 
     public boolean isExit() {
         return false;
